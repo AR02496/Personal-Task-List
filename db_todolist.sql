ce7aa3cc5cad65263855b6ae4b0bd8d3ed20324d
ratihsatyawati <ratihst93@gmail.com>
2025-12-29 09:16:27 +0800

    Add files via upload
    menambahkan database

---
+++ db_todolist.sql
@@ -0,0 +1,25 @@
+create database todolist; 
+
+use todolist;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
+);
+
+
+use todolist;
+CREATE TABLE tasks (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    task VARCHAR(255) NOT NULL,
+    description TEXT,
+    deadline DATE,
+    status TINYINT(1) DEFAULT 0,
+    CONSTRAINT fk_user_list
+        FOREIGN KEY (user_id) REFERENCES users(id)
+        ON DELETE CASCADE
+);
+
