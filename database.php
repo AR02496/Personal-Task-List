f2062e69a3459c507a94940fd7fa15d6278fc872
AR02496 <creamyblaze61@gmail.com>
2026-01-04 20:02:22 +0700

    Update database.php
    menambahkan dokumentasi dan inkonsistensi

--- database.php
+++ database.php
@@ -1,16 +1,19 @@
 <?php
-
+// Konfigurasi koneksi ke database MySQL
 $host = 'localhost';
 $port = 3306;
-$username = 'root';    
-$password = 'AsuKayang69';        
-$dbname = 'todolist';  
+$username = 'root';
+$password = ''; // GANTI password sesuai dengan local instance masing-masing
+$dbname = 'todolist'; 
 
+// Membuat koneksi
 $koneksi = mysqli_connect($host, $username, $password, $dbname, $port);
 
+// Cek koneksi
 if (!$koneksi) {
     die("Error koneksi: " . mysqli_connect_error());
 }
 
+// Set charset untuk mendukung UTF-8
 mysqli_set_charset($koneksi, "utf8mb4");
 ?>
