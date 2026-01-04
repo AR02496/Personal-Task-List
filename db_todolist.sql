0d756386d348d995e3d8c63438c08c15fd55cc0f
AR02496 <creamyblaze61@gmail.com>
2026-01-04 19:56:48 +0700

    Update db_todolist.sql

--- db_todolist.sql
+++ db_todolist.sql
@@ -1,4 +1,4 @@
-create database todolist; 
+CREATE DATABASE IF NOT EXISTS todolist; 
 
 use todolist;
 
@@ -9,14 +9,13 @@ CREATE TABLE users (
     password VARCHAR(255) NOT NULL
 );
 
-use todolist;
 CREATE TABLE tasks (
     id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT NOT NULL,
     task VARCHAR(255) NOT NULL,
     description TEXT,
     deadline DATE,
-    status TINYINT DEFAULT 0, --TINYINT (1) menjadi TINYINT
+    status TINYINT DEFAULT 0, 
     CONSTRAINT fk_user_list
         FOREIGN KEY (user_id) REFERENCES users(id)
         ON DELETE CASCADE
